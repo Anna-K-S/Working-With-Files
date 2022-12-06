d923513feecacf35a0d2cbd97e6493c8959b3132
Anna-K-S <anna.sergeevna1622@gmail.com>
2022-12-07 02:17:26 +0300

    Writing and reading text file in Python

---
+++ reading_text_files.py
@@ -0,0 +1,8 @@
+# with open(r'C:\Users\р\OneDrive\Desktop\new_text.txt') as new_text:
+#     pass
+colors_list = []
+with open(r'C:\\Users\\р\\OneDrive\\Desktop\\new_text.txt') as new_text:
+    for color in new_text:
+        colors_list.append(color.strip('\n'))
+        # '.strip' - обрезает символ указанный в параметре для строк
+print(colors_list)
---
+++ writing_text.py
@@ -0,0 +1,12 @@
+# colors_list = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo',
+#                'violet', 'dark green']
+# with open('C:\\Users\р\OneDrive\Desktop\\new_text.txt', 'w') as new_text:
+#     for color in colors_list:
+#         print(color, file=new_text)
+#
+# colors_list = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo',
+#                'violet', 'dark green']
+# with open('C:\\Users\р\OneDrive\Desktop\\new_text.txt', 'a') as new_text:
+#     print('white', file=new_text)
+#     print('pink', file=new_text)
+
